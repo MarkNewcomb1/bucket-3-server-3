a35448f1bd6b55a6f97f54ff4a417bb8d5b0727b
MarkNewcomb1 <wlmmn@hotmail.com>
2018-07-31 08:15:58 -0600

    uses top-level data structure and returns 404 on no record found

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const csvToJson = require('convert-csv-to-json');
 const port = parseInt(process.env.PORT || 9000);
 app.use(cors());
-const students = csvToJson.fieldDelimiter(',').getJsonFromCsv('students.csv');
+const data = csvToJson.fieldDelimiter(',').getJsonFromCsv('students.csv');
 
 function findById(data, id) {
     for (let i = 0; i < data.length; i++) {
@@ -16,20 +16,20 @@ function findById(data, id) {
 }
 
 app.get('/', (request, response) => {
-    response.json(students);
+    response.json({data: data});
 })
 
 app.get("/:id", function (request, response) {
-    var record = findById(students, request.params.id);
+    var record = findById(data, request.params.id);
     if (!record){
-        response.status = 404;
-        response.json({
+        // response.status = 404;
+        response.status(404).send({
             error: {
                 message: "No record found!"
             }
-        });
+        })
     } else {
-        response.json({students: record});
+        response.json({data: record});
     }
 });
 
