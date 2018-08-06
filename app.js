7bc197316ec86ccbed88ef07d140c1032b75118a
MarkNewcomb1 <wlmmn@hotmail.com>
2018-08-06 08:07:27 -0600

    removed comments

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.get('/', (request, response) => {
 app.get("/:id", function (request, response) {
     var record = findById(data, request.params.id);
     if (!record){
-        // response.status = 404;
         response.status(404).send({
             error: {
                 message: "No record found!"
